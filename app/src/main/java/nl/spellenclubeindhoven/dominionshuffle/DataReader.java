d8049db53df299a72991d01feb142a00bdda71be
Sören Glimm <git@uncleowen.de>
2015-12-26 20:21:40 +0100

    Replace gzipped data files with uncompressed plain text files
    Since apk files are actually zip files, they are already compressed.
    So this doesn't increase apk file size. (Actually, the resulting apk is
    smaller by a few bytes)
    
    Storing plain text files in git is better, because that way you
    can see the changes in git diff.

--- app/src/main/java/nl/spellenclubeindhoven/dominionshuffle/DataReader.java
+++ app/src/main/java/nl/spellenclubeindhoven/dominionshuffle/DataReader.java
@@ -27,7 +27,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.zip.GZIPInputStream;
 
 import nl.spellenclubeindhoven.dominionshuffle.data.CardSelector;
 import nl.spellenclubeindhoven.dominionshuffle.data.Data;
@@ -52,7 +51,7 @@ public class DataReader {
 		if(dominionData != null) return false;
 
 		try {
-			InputStream inputStream = new GZIPInputStream(application.getResources().openRawResource(R.raw.data));
+			InputStream inputStream = application.getResources().openRawResource(R.raw.data);
 			dominionData = Data.read(readStringFromStream(inputStream));
 		} catch (Exception ignore) {
 			ignore.printStackTrace();
